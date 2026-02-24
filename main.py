4ef6c1ee4c4b4726b409996649eac015de56a670
Martin Šeleng <pakoselo@users.noreply.github.com>
2026-02-24 14:39:42 +0100

    reload and history functions added

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import FileResponse
+from contextlib import asynccontextmanager
 
 from enum import Enum
 from pydantic import BaseModel
@@ -10,12 +11,7 @@ from datetime import datetime
 
 import uvicorn
 
-
-app = FastAPI()
-
-app.mount("/static", StaticFiles(directory="static"), name="static")
-app.mount("/images", StaticFiles(directory="images"), name="images")
-templates = Jinja2Templates(directory="templates")
+import pickle
 
 
 class StatusEnum(str, Enum):
@@ -40,7 +36,6 @@ class ModelStatus(BaseModel):
     class Config:  
         use_enum_values = True
 
-
 models = [
     DicrisModel(name = 'Model 1', status = StatusEnum.undefined, time = datetime.now()),
     DicrisModel(name = 'Model 2', status = StatusEnum.undefined, time = datetime.now()),
@@ -49,8 +44,33 @@ models = [
     DicrisModel(name = 'Model 5', status = StatusEnum.undefined, time = datetime.now()),
     DicrisModel(name = 'Model 6', status = StatusEnum.undefined, time = datetime.now()),
 ]
-
 favicon_path = '/images/favicon.ico'
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    model = DicrisModel(name='', status=StatusEnum.undefined, time=datetime.now())
+    with open("models.bin", 'rb') as f:
+        try:
+            while True:
+                model = pickle.load(f)
+                for idx, m in enumerate(models):
+                    if model.name == m.name:
+                        models[idx] = model
+                        break
+        except EOFError:
+            f.close()
+            pass
+    #app.state.models = models
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/images", StaticFiles(directory="images"), name="images")
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get('/favicon.ico', include_in_schema=False)
 async def favicon():
@@ -73,13 +93,15 @@ async def get_model(name:str):
 @app.post("/models", status_code=201)
 async def add_status_model(model_status: ModelStatus):
     model = DicrisModel(name='', status=StatusEnum.undefined, time=datetime.now())
-
+    #models = app.state.models
     for idx, m in enumerate(models):
         if model_status.name == m.name:
             m.status = model_status.status
             m.time = datetime.now()
             models[idx] = m
             model = m
+            pickle.dump(m, open("models.bin", "ab+"))
+            break
     if model.name =='':
         return 
     else:
@@ -95,5 +117,7 @@ async def home(request: Request):
         request=request, name="models.html", context=context
     )
 
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, ssl_keyfile="/etc/ssl/harica/dicris.sk.key", ssl_certfile="/etc/ssl/harica/dicris.sk.pem")
+    #uvicorn.run("main:app", host="0.0.0.0", port=8000)
